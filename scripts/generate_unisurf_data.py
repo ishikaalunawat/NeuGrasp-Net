17486c784edae7023f3e05e7462d5a443ed1ce2a
ishikaa25 <ishikaalunawat@gmail.com>
2023-03-09 09:57:12 +0100

    diff scenes

--- scripts/generate_unisurf_data.py
+++ scripts/generate_unisurf_data.py
@@ -3,5 +3,5 @@ from tqdm import tqdm
 from pathlib import Path
 # (Path(os.getcwd()) / "unisurf_data_pc").mkdir(parents=True)
 
-for i in tqdm(range(0, 5)): # 0 to (num_scenes-1)
+for i in tqdm(range(5, 10)): # 0 to (num_scenes-1)
     os.system("python scripts/generate_data_parallel.py --scene pile --object-set pile/train --num-grasps 5 --save-scene ../potato-net/data/scene{0:03d} --random".format(i))
