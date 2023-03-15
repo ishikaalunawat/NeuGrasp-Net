1fdf1a80a081aa1cd8323141b6b3ede88599b7ce
Snehal Jauhri <snehal.jauhri@gmail.com>
2023-03-15 10:57:17 +0100

    updated slurm training

--- scripts/launch_slurm_train_giga.py
+++ scripts/launch_slurm_train_giga.py
@@ -13,28 +13,28 @@ N_SEEDS = 1
 # else:
 #     N_EXPS_IN_PARALLEL = 3
 
-N_CORES = 5 #N_EXPS_IN_PARALLEL # HRZ nodes have 96 cores
+N_CORES = 16 #N_EXPS_IN_PARALLEL # HRZ nodes have 96 cores
 # MEMORY_SINGLE_JOB = 1000
 # MEMORY_PER_CORE = N_EXPS_IN_PARALLEL * MEMORY_SINGLE_JOB // N_CORES
-MEMORY_PER_CORE = 3500
-PARTITION = 'dgx' # 'amd2,amd'  # 'amd', 'rtx'
-GRES = 'gpu:1' # if USE_CUDA else None  # gpu:rtx2080:1, gpu:rtx3080:1
+MEMORY_PER_CORE = 4000
+# PARTITION = 'dgx' # 'amd2,amd'  # 'amd', 'rtx'
+GRES = 'gpu' # if USE_CUDA else None  # gpu:rtx2080:1, gpu:rtx3080:1
 CONDA_ENV = 'GIGA-6DoF'
 
 launcher = Launcher(
-    exp_name='train_HiRes64_giga',
+    exp_name='train_HiRes64_6Dw_giga_GPG_balanced',
     exp_file='train_giga', # local path without .py
     # exp_file='/work/home/sj93qicy/IAS_WS/potato-net/GIGA-6DoF/scripts/generate_data_gpg_parallel', # without .py
-    # project_name='project01907',  # for hrz cluster
+    project_name='project01907',  # for hrz cluster
     n_seeds=N_SEEDS,
     # n_exps_in_parallel=N_EXPS_IN_PARALLEL,
     n_cores=N_CORES,
     memory_per_core=MEMORY_PER_CORE,
-    days=2,
-    hours=0,
-    minutes=0,
+    days=1,
+    hours=23,
+    minutes=59,
     seconds=0,
-    partition=PARTITION,
+    # partition=PARTITION,
     gres=GRES,
     conda_env=CONDA_ENV,
     use_timestamp=True,
@@ -45,11 +45,12 @@ launcher = Launcher(
 # Experiment configs (In this case, they are all argparse arguments for the main python file)
 launcher.add_experiment(
     net="giga_hr",
-    dataset="/home/jauhri/IAS_WS/potato-net/GIGA-TSDF/GIGA-6DoF/data/pile/data_pile_train_constructed_4M_HighRes_radomized_views",
-    dataset_raw="/home/jauhri/IAS_WS/potato-net/GIGA-TSDF/GIGA-6DoF/data/pile/data_pile_train_random_raw_4M_radomized_views",
-    epochs=30,
-    batch_size=128,
-    num_workers=N_CORES
+    dataset="/work/scratch/sj93qicy/potato-net/data/pile/data_pile_train_constructed_4M_HighRes_radomized_views",
+    dataset_raw="/work/scratch/sj93qicy/potato-net/data/pile/data_pile_train_random_raw_4M_GPG_60",
+    epochs=35,
+    batch_size=16,
+    num_workers=10,
+    epoch_length_frac=0.5,
     )
 
 launcher.run(LOCAL, TEST)
