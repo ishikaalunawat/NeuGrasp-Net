07937b0808a0cdf9d0d244e1b3e3d58bcede989c
ishikaa25 <ishikaalunawat@gmail.com>
2023-08-09 18:37:11 +0200

    viz

---
+++ scripts/contact_sim_grasp_save_scene.py
@@ -0,0 +1,110 @@
+import json
+import argparse
+import numpy as np
+from pathlib import Path
+from vgn.utils.transform import Transform, Rotation
+from vgn.grasp import Grasp
+from vgn.experiments import contact_clutter_removal_single
+from vgn.utils.implicit import get_scene_from_mesh_pose_list
+
+raw_root = '/media/hypatia/6903154a-554e-4ca5-987c-4a24f3250e97/home/hypatia/6D-DAAD/GIGA/data/pile/data_pile_train_random_raw_4M_radomized_views/mesh_pose_list/'
+data_root = "/media/hypatia/6903154a-554e-4ca5-987c-4a24f3250e97/home/hypatia/6D-DAAD/GIGA/"
+
+T_try = np.array(
+    [[0, -1, 0, 0],
+     [0, 0, 1, 0],
+     [-1, 0, 0, 0],
+     [0, 0, 0, 1]]
+)
+gripper_width = 0.08
+
+def load_data(scene_id, result_path, pc_path):
+    pc_full = np.load(pc_path + scene_id + '.npz', allow_pickle=True)['pc'] # OURS
+    results = np.load(result_path + f'predictions_{scene_id}.npz', allow_pickle=True)
+    pred_grasps_cam, scores = results['pred_grasps_cam'].item()[-1], results['scores'].item()[-1]
+    mesh_pose_list = np.load(raw_root + scene_id + '.npz', allow_pickle=True)['pc']
+    scene_mesh = get_scene_from_mesh_pose_list(mesh_pose_list, data_root=data_root)
+    return (pred_grasps_cam, scores), pc_full, scene_mesh
+
+def pre_process_grasp(grasps_info):
+    grasps_info = grasps_info[0]
+
+    grasps = []
+    for i in range(len(grasps_info)):
+        sample_grasp = grasps_info[i]
+        sample_grasp = np.dot(sample_grasp, T_try)
+
+        grasp_rot, grasp_pos = sample_grasp[:3, :3], sample_grasp[:3, 3]
+        grasp_rot = Rotation.from_matrix(grasp_rot)
+        grasp = Grasp(Transform(grasp_rot, grasp_pos), gripper_width) # make grasp with width 0.08 (max gripper width)?
+
+        grasp_frame_rot =  grasp.pose.rotation * Rotation.from_euler('Y', -0.5*np.pi) #* Rotation.from_euler('Z', np.pi)
+        # grasp_tf = Transform(grasp_frame_rot, grasp_pos).as_matrix()
+        grasp_pos[2] -= 0.015
+        grasp_pos[1] += 0.03
+        grasp = Grasp(Transform(grasp_frame_rot, grasp_pos), gripper_width) # make grasp
+        grasps.append(grasp)
+
+    return grasps
+
+def main(args):
+    #wandb.init(config=args, project="6dgrasp", entity="irosa-ias")
+    grasps_info, pc, scene_mesh = load_data(args.scene_id, args.result_path, args.pc_path)
+    grasps_info = pre_process_grasp(grasps_info)
+
+    results = {}
+    for n in range(args.num_rounds):
+        args.seed = np.random.randint(3000)
+        save_dir = args.save_dir / f'round_{n:03d}'
+        results[n], visual_mesh = contact_clutter_removal_single.run(grasps_info=grasps_info,
+                                                                    pc=pc,
+                                                                    scene_mesh=scene_mesh,
+                                                                    save_dir=save_dir,
+                                                                    scene=args.scene,
+                                                                    object_set=args.object_set,
+                                                                    num_objects=args.num_objects,
+                                                                    n=args.num_view,
+                                                                    seed=args.seed,
+                                                                    sim_gui=args.sim_gui,
+                                                                    add_noise=args.add_noise,
+                                                                    sideview=args.sideview)
+        print(f'Round {n} finished, result: {results[n]}')
+        if args.viz:
+            visual_mesh.export(args.save_dir / 'scene_'.format(n), 'obj')
+        #wandb.log({'Grasps' : wandb.Object3D(open(args.save_dir + '/scene_{}.obj' % n))})
+
+    with open(args.save_dir / 'results_contact_grasp_net.json', 'w') as f:
+        json.dump(results, f, indent=2)
+    
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--scene-id", type=str, required=True)
+    parser.add_argument("--result-path", type=str, default='/home/hypatia/6D-DAAD/contact_graspnet/results/')
+    parser.add_argument("--pc-path", type=str, 
+                        default='/media/hypatia/6903154a-554e-4ca5-987c-4a24f3250e97/home/hypatia/6D-DAAD/GIGA/data/pile/data_pile_train_constructed_4M_HighRes_radomized_views_no_table/point_clouds/')
+    # parser.add_argument("--type", type=str, required=True)
+    parser.add_argument("--save-dir", type=Path, default=Path('results_contact_grasp_net'))
+    parser.add_argument("--scene",type=str,choices=["pile", "packed"],default="pile")
+    parser.add_argument("--object-set", type=str, default="blocks")
+    parser.add_argument("--num-objects", type=int, default=5)
+    parser.add_argument("--num-view", type=int, default=1)
+    parser.add_argument("--num-rounds", type=int, default=10)
+    parser.add_argument("--seed", type=int, default=42)
+    parser.add_argument("--sim-gui", action="store_true")
+    # parser.add_argument("--grad-refine", action="store_true")
+    parser.add_argument("--qual-th", type=float, default=0.9)
+    parser.add_argument("--best",action="store_true",help="Whether to use best valid grasp (or random valid grasp)")
+    parser.add_argument("--force",action="store_true",help="When all grasps are under threshold, force the detector to select the best grasp")
+    parser.add_argument("--add-noise",type=str,default='',help="Whether add noise to depth observation, trans | dex | norm | ''")
+    parser.add_argument("--sideview",action="store_true",help="Whether to look from one side")
+    parser.add_argument("--simple-constrain",action="store_true",help="Whether to contrain grasp from backward")
+    parser.add_argument("--res", type=int, default=40)
+    parser.add_argument("--out-th", type=float, default=0.5)
+    parser.add_argument("--silence",action="store_true",help="Whether to disable tqdm bar")
+    parser.add_argument("--select-top",action="store_true",help="Use top heuristic")
+    parser.add_argument("--viz",action="store_true",help="visualize and save affordance")
+
+    args = parser.parse_args()
+    main(args)
---
+++ src/vgn/experiments/contact_clutter_removal_single.py
@@ -0,0 +1,100 @@
+import collections
+import argparse
+from datetime import datetime
+import os
+
+
+import numpy as np
+import trimesh
+from vgn.utils import visual
+
+from vgn import io#, vis
+from vgn.grasp import *
+from vgn.simulation import ClutterRemovalSim
+from vgn.utils.transform import Rotation, Transform
+from vgn.utils.implicit import get_mesh_pose_list_from_world, get_scene_from_mesh_pose_list
+
+MAX_CONSECUTIVE_FAILURES = 2
+
+
+State = collections.namedtuple("State", ["tsdf", "pc"])
+
+def visualize_grasps(scene_mesh, grasps, pc_full):
+    # Debug: Viz grasps
+    seen_grasps_scene = trimesh.Scene()
+    # scene_pc = trimesh.points.PointCloud(pc_full, colors=np.tile(np.array([0, 0, 0, 1]), (len(pc_full), 1)))
+    seen_grasp_mesh_list = [visual.grasp2mesh(g) for g in grasps]
+    for i, g_mesh in enumerate(seen_grasp_mesh_list):
+        seen_grasps_scene.add_geometry(g_mesh, node_name=f'grasp_{i}')
+    composed_scene = trimesh.Scene([scene_mesh, seen_grasps_scene])#, unseen_grasps_scene])
+    # composed_scene.add_geometry(trimesh.creation.axis())
+    return composed_scene
+
+def run( grasps_info, pc, scene_mesh, save_dir, scene, object_set, num_objects=5, n=6, N=None, seed=1, sim_gui=False, add_noise=False,
+        sideview=False, resolution=40, silence=False, save_freq=8, visualize = True):
+    """Run several rounds of simulated clutter removal experiments.
+
+    Each round, m objects are randomly placed in a tray. Then, the grasping pipeline is
+    run until (a) no objects remain, (b) the planner failed to find a grasp hypothesis,
+    or (c) maximum number of consecutive failed grasp attempts.
+    """
+    os.makedirs(save_dir, exist_ok=True)
+    sim = ClutterRemovalSim(scene, object_set, gui=sim_gui, seed=seed, add_noise=add_noise, sideview=sideview, save_dir=save_dir)
+    cnt = 0
+    success = 0
+    left_objs = 0
+    total_objs = 0
+    cons_fail = 0
+    no_grasp = 0
+
+    sim.reset(num_objects)
+
+    total_objs += sim.num_objects
+    consecutive_failures = 1
+    last_label = None
+    trial_id = -1
+
+    while sim.num_objects > 0 and consecutive_failures < MAX_CONSECUTIVE_FAILURES:
+        trial_id += 1
+        timings = {}
+        grasps = grasps_info
+        # scan the scene
+        # tsdf, pc, timings["integration"] = sim.acquire_tsdf(n=n, N=N, resolution=40)
+        # state = argparse.Namespace(tsdf=tsdf, pc=pc)
+        # if resolution != 40:
+        #     extra_tsdf, _, _ = sim.acquire_tsdf(n=n, N=N, resolution=resolution)
+        #     state.tsdf_process = extra_tsdf
+
+        if len(pc)==0:
+            break  # empty point cloud, abort this round TODO this should not happen
+
+        ## TODO: Figure out visual_mesh
+        if visualize:
+            visual_mesh = visualize_grasps(scene_mesh, grasps, pc)
+            # grasps, scores, timings["planning"], visual_mesh = grasp_plan_fn(state, scene_mesh)
+        else:
+            visual_mesh = None
+            # grasps, scores, timings["planning"] = grasp_plan_fn(state)
+
+        if len(grasps) == 0:
+            no_grasp += 1
+            break  # no detections found, abort this round
+
+        # execute grasp
+        # grasp, score = grasps[0], scores[0]
+        grasp = grasps[0] # Same as GIGA (best)
+        label, _ = sim.execute_grasp(grasp, allow_contact=True)
+        cnt += 1
+        if label != Label.FAILURE:
+            success += 1
+
+        if last_label == Label.FAILURE and label == Label.FAILURE:
+            consecutive_failures += 1
+        else:
+            consecutive_failures = 1
+        if consecutive_failures >= MAX_CONSECUTIVE_FAILURES:
+            cons_fail += 1
+        last_label = label
+    left_objs += sim.num_objects
+
+    return (success, cnt, total_objs), visual_mesh
